6969f2dd51e56be2e8d14f534e45d29489e163fe
Kevin Friedrichs <friedrichs.kevin@gmail.com>
2023-08-20 18:20:12 +0200

    Install Chakre Ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import "./App.css";
+import { Button, ButtonGroup } from "@chakra-ui/react";
 
 function App() {
-  return <div></div>;
+  return <Button colorScheme="blue">Button</Button>;
 }
 
 export default App;
