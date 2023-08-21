0a52fc4d5234ac28eaab338a593ba5d8e52e3d16
Kevin Friedrichs <friedrichs.kevin@gmail.com>
2023-08-21 11:35:24 +0200

    Added Login component

--- src/components/NavLogin.tsx
+++ src/components/NavLogin.tsx
@@ -1,6 +1,7 @@
-import { Button, HStack, Text, Link } from "@chakra-ui/react";
+import { Button, HStack } from "@chakra-ui/react";
 import { BiSolidUser } from "react-icons/bi";
 import { MdLogin } from "react-icons/md";
+import { Link as ReactRouterLink } from "react-router-dom";
 
 interface Props {
   onLogin: () => void;
@@ -10,28 +11,33 @@ interface Props {
 const NavLogin = ({ onLogin, onSignup }: Props) => {
   return (
     <HStack>
-      <Button
-        variant="solid"
-        size="sm"
-        leftIcon={<MdLogin size={20} />}
-        fontSize={14}
-        color="green.400"
-        bg="white"
-        onClick={onLogin}
-      >
-        Login
-      </Button>
-      <Button
-        variant="solid"
-        size="sm"
-        leftIcon={<BiSolidUser size={20} />}
-        fontSize={14}
-        color="green.400"
-        bg="white"
-        onClick={onSignup}
-      >
-        Sign Up
-      </Button>
+      <ReactRouterLink to="/login">
+        <Button
+          variant="solid"
+          size="sm"
+          leftIcon={<MdLogin size={20} />}
+          fontSize={14}
+          color="green.400"
+          bg="white"
+          onClick={onLogin}
+        >
+          Login
+        </Button>
+      </ReactRouterLink>
+
+      <ReactRouterLink to="/signup">
+        <Button
+          variant="solid"
+          size="sm"
+          leftIcon={<BiSolidUser size={20} />}
+          fontSize={14}
+          color="green.400"
+          bg="white"
+          onClick={onSignup}
+        >
+          Sign Up
+        </Button>
+      </ReactRouterLink>
     </HStack>
   );
 };
--- src/components/PageLogin.tsx
+++ src/components/PageLogin.tsx
@@ -1,7 +1,45 @@
-import React from "react";
+import { Button } from "@chakra-ui/button";
+import {
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Box,
+  Center,
+} from "@chakra-ui/react";
+import { FormEvent, useRef } from "react";
+import { BiSolidUser } from "react-icons/bi";
+import { FaKey } from "react-icons/fa";
 
 const PageLogin = () => {
-  return <div>PageLogin</div>;
+  const usernameRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    console.log(usernameRef.current?.value + " " + passwordRef.current?.value);
+  };
+
+  return (
+    <Center marginTop={10}>
+      <form onSubmit={handleSubmit}>
+        <InputGroup>
+          <Input type="text" placeholder="Username" ref={usernameRef} />
+          <InputLeftElement>
+            <BiSolidUser size={20} color="gray" />
+          </InputLeftElement>
+        </InputGroup>
+
+        <InputGroup>
+          <Input type="password" placeholder="Password" ref={passwordRef} />
+          <InputLeftElement>
+            <FaKey size={16} color="gray" />
+          </InputLeftElement>
+        </InputGroup>
+
+        <Input type="submit" value="submit" bg="green.400" color="white" />
+      </form>
+    </Center>
+  );
 };
 
 export default PageLogin;
